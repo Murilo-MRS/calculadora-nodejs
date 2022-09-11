d29469c3950237e6df8988933d2902d5bd422361
Murilo-MRS <murilosantana1993@gmail.com>
2022-09-11 17:45:31 -0300

    finalizado calc

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-const operations = require('./operations');
+const operations = require("./operations");
+const readline = require("readline-sync");
+
+const num1 = readline.questionInt("Digite um numero:");
+const oper = readline.question("Digite a operação: +, -, *, / ");
+const num2 = readline.questionInt("Digite um numero:");
+
+switch (oper) {
+    case '+':
+        operations.sum(num1, num2)
+        break;
+    case '-':
+        operations.sub(num1, num2)
+        break;
+    case '*':
+        operations.multi(num1, num2)
+        break;
+    case '/':
+        operations.div(num1, num2)
+        break;
+
+    default:
+        console.log("Opa, operação inexistente.");
+        break;
+}
--- operations.js
+++ operations.js
@@ -1,7 +1,7 @@
-const sum = (num1, num2) => console.log(`${num1} + ${num2} ${num1 + num2}`);
-const sub = (num1, num2) => console.log(`${num1} - ${num2} ${num1 - num2}`);
-const multi = (num1, num2) => console.log(`${num1} * ${num2} ${num1 * num2}`);
-const div = (num1, num2) => console.log(`${num1} / ${num2} ${num1 / num2}`);
+const sum = (num1, num2) => console.log(`${num1} + ${num2} = ${num1 + num2}`);
+const sub = (num1, num2) => console.log(`${num1} - ${num2} = ${num1 - num2}`);
+const multi = (num1, num2) => console.log(`${num1} * ${num2} = ${num1 * num2}`);
+const div = (num1, num2) => console.log(`${num1} / ${num2} = ${num1 / num2}`);
 
 module.exports = {
     sum,
